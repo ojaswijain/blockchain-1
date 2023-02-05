5d861cda5b8575a1060788f457596bd64ceee7ad
Ojaswi Jain <200050092@iitb.ac.in>
2023-02-05 10:07:16 +0530

    first commit

--- utils.py
+++ utils.py
@@ -1,3 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  4 12:03:13 2023
+
+@author: ojaswi
+
+Building a discrete event simulator for a P2P cryptocurrency network
+"""
+
+from datetime import datetime
+from hashlib import sha256
+import json
 import numpy as np
 
 def write_to_file(filename, data):
